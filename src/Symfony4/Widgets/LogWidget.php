71434271b9912bf7f534334f1919e0b3381482cf
Vitaliy <theyamshikov@yandex.ru>
2022-05-18 18:49:20 +0600

    upd

--- src/Symfony4/Widgets/LogWidget.php
+++ src/Symfony4/Widgets/LogWidget.php
@@ -5,9 +5,23 @@ namespace ZnLib\Console\Symfony4\Widgets;
 class LogWidget extends BaseWidget
 {
 
+    private $pretty = false;
+    private $titleLength = 30;
+
+    public function isPretty(): bool
+    {
+        return $this->pretty;
+    }
+
+    public function setPretty(bool $pretty): void
+    {
+        $this->pretty = $pretty;
+    }
+
     public function start(string $message)
     {
-        $this->output->write("<fg=white>{$message}... </>");
+        $dots = $this->generatePadding(mb_strlen($message));
+        $this->output->write("<fg=white>{$message} {$dots} </>");
     }
 
     public function finishSuccess(string $message = 'OK')
@@ -18,5 +32,18 @@ class LogWidget extends BaseWidget
     public function finishFail(string $message = 'FAIL')
     {
         $this->output->writeln("<fg=red>{$message}</>");
+    }
+
+    protected function generatePadding(int $messageLength): string
+    {
+        if ($this->isPretty()) {
+            $rest = $this->titleLength - $messageLength;
+        } else {
+            $rest = 3;
+        }
+        if ($rest < 3) {
+            $rest = 3;
+        }
+        return str_repeat('.', $rest);
     }
 }
