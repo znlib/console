c5c8c87f331beb07dfa1c1d2d4c5d3dc1f463905
Vitaliy <theyamshikov@yandex.ru>
2022-06-17 20:22:05 +0600

    upd

---
+++ src/Domain/Libs/BundleLoaders/ConsoleLoader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ZnLib\Console\Domain\Libs\BundleLoaders;
+
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
+use ZnCore\Base\Libs\App\Interfaces\ConfigManagerInterface;
+use ZnCore\Base\Libs\App\Loaders\BundleLoaders\BaseLoader;
+
+class ConsoleLoader extends BaseLoader
+{
+
+    public function loadAll(array $bundles): array
+    {
+        $config = [];
+        foreach ($bundles as $bundle) {
+            $loadedConfig = $this->load($bundle);
+            $config = ArrayHelper::merge($config, $loadedConfig);
+        }
+
+        $configManager = $this->getContainer()->get(ConfigManagerInterface::class);
+        $configManager->set('consoleCommands', $config);
+
+        return $config ? ['consoleCommands' => $config] : [];
+    }
+}
