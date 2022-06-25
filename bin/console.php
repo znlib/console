4cf73a5b101ca8d34ed3a461019ee61911e1c571
Vitaliy <theyamshikov@yandex.ru>
2022-06-25 19:30:58 +0600

    upd

---
+++ bin/console.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Container\Container;
+use Symfony\Component\Console\Application;
+use ZnCore\Base\App\Interfaces\AppInterface;
+use ZnCore\Base\App\Libs\ZnCore;
+use ZnCore\Base\Container\Interfaces\ContainerConfiguratorInterface;
+use ZnCore\Base\DotEnv\Domain\Libs\DotEnv;
+use ZnCore\Base\DotEnv\Domain\Libs\DotEnvLoader;
+use ZnCore\Base\EventDispatcher\Interfaces\EventDispatcherConfiguratorInterface;
+use ZnCore\Base\FileSystem\Helpers\FilePathHelper;
+use ZnLib\Console\Domain\Libs\ConsoleApp;
+
+define('MICRO_TIME', microtime(true));
+
+require __DIR__ . '/../../../../vendor/autoload.php';
+
+$container = new Container();
+$znCore = new ZnCore($container);
+$znCore->init();
+
+/** @var ContainerConfiguratorInterface $containerConfigurator */
+$containerConfigurator = $container->get(ContainerConfiguratorInterface::class);
+
+/** @var EventDispatcherConfiguratorInterface $eventDispatcherConfigurator */
+$eventDispatcherConfigurator = $container->get(EventDispatcherConfiguratorInterface::class);
+
+if ($argv[1] == 'init:run') {
+
+    $containerConfigurator->singleton(AppInterface::class, \ZnLib\Console\Domain\Libs\NullConsoleApp::class);
+    /** @var AppInterface $appFactory */
+    $appFactory = $container->get(AppInterface::class);
+    $appFactory->addBundles([
+        \ZnLib\Init\Bundle::class,
+    ]);
+    $appFactory->init();
+//    dd($container->get(\ZnCore\Base\ConfigManager\Interfaces\ConfigManagerInterface::class));
+} else {
+    //$mainEnv = DotEnv::loadFromFile(DotEnv::ROOT_PATH . '/.env');
+    $loader = new DotEnvLoader();
+    $mainEnv = $loader->loadFromFile(FilePathHelper::rootPath() . '/.env');
+    $consoleAppClass = $mainEnv['CONSOLE_APP_CLASS'] ?? ConsoleApp::class;
+    $containerConfigurator->singleton(AppInterface::class, $consoleAppClass);
+
+    /** @var AppInterface $appFactory */
+    $appFactory = $container->get(AppInterface::class);
+
+    $appFactory->addBundles([
+        \ZnTool\Package\Bundle::class,
+        \ZnDatabase\Base\Bundle::class,
+        \ZnDatabase\Tool\Bundle::class,
+        \ZnDatabase\Fixture\Bundle::class,
+        \ZnDatabase\Migration\Bundle::class,
+        \ZnTool\Generator\Bundle::class,
+        \ZnTool\Stress\Bundle::class,
+        \ZnBundle\Queue\Bundle::class,
+        \ZnCore\Base\DotEnv\Bundle::class,
+    ]);
+    $appFactory->init();
+}
+
+/** @var Application $application */
+$application = $container->get(Application::class);
+$application->run();
---
+++ src/Domain/Libs/NullConsoleApp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZnLib\Console\Domain\Libs;
+
+use ZnLib\Console\Symfony4\Base\BaseConsoleApp;
+
+class NullConsoleApp extends BaseConsoleApp
+{
+
+}
