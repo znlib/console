12b723260b349163f6e36ffdc2f337b38dd5ee12
ZnDeveloper <zndev@ya.ru>
2022-07-03 17:24:42 +0600

    upd

--- bin/console.php
+++ bin/console.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Container\Container;
+use ZnCore\Base\Container\Libs\Container;
 use Symfony\Component\Console\Application;
 use ZnCore\Base\App\Interfaces\AppInterface;
 use ZnCore\Base\App\Libs\ZnCore;
