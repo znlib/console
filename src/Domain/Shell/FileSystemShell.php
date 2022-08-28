98e9c5c3a0530621e166df9ef307e80782c82706
vitaliy <zndev@ya.ru>
2022-08-28 16:57:42 +0600

    upd

--- src/Domain/Shell/FileSystemShell.php
+++ src/Domain/Shell/FileSystemShell.php
@@ -25,7 +25,7 @@ class FileSystemShell extends BaseShellNew
         return $items;
     }
 
-    /*public function filterItem(array $item): bool
+    public function filterItem(array $item): bool
     {
         return !in_array($item['fileName'], ['.', '..']);
     }
@@ -54,5 +54,5 @@ class FileSystemShell extends BaseShellNew
             return $item;
         }
         return null;
-    }*/
+    }
 }
